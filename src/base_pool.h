4c42687f217da263fc5f4fe62eb0961c39c5404f
YukunJ <yukunj@andrew.cmu.edu>
2023-04-03 18:48:50 -0400

    build: setup Makefile

---
+++ src/base_pool.h
@@ -0,0 +1,11 @@
+/**
+ * @file base_pool.h
+ * @expectation this header file should be compatible to compile in C++
+ * program on Linux
+ * @init_date Apr 03 2023
+ *
+ * This is a header file specifying the interface for
+ * all types of threadpool implementations in this project
+ */
+
+#define MSG "Hello World from Zorro!"
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cppJ
+ * @expectation this header file should be compatible to compile in C++
+ * program on Linux
+ * @init_date Apr 03 2023
+ *
+ * This is the main program entry for performance benchmarking
+ * run the program by './run_pool [extra parameters specification see below]'
+ */
+
+#include <iostream>
+
+#include "base_pool.h"
+
+int main(int argc, char* argv[]) {
+  std::cout << MSG << std::endl;
+  return 0;
+}
