0c25cad164d9e8a01646217e59eab861a48312f0
LeoGuo98 <leoguo1234567890@gmail.com>
2023-04-26 17:42:46 -0400

    added basic file for stealing

---
+++ src/local_fine_pool_naive_steal.cpp
@@ -0,0 +1,109 @@
+/**
+ * @file local_fine_pool.cpp
+ * @expectation this implementation file should be compatible to compile in C++
+ * program on Linux
+ * @init_date Apr 18 2023
+ *
+ * This is an implementation file that implements the second version of
+ * threadpool using thread-local work queue but fine locking
+ */
+
+#include "local_fine_pool_naive_steal.h"
+
+#include <iostream>
+
+LocalFinePoolNaiveSteal::LocalFinePoolNaiveSteal(int concurrency, PoolType pool_type)
+    : BasePool(concurrency, pool_type) {
+  for (int i = 0; i < concurrency_; i++) {
+    // create padded resources
+    auto r = std::make_unique<PaddedResourceFine>();
+    resources_.push_back(std::move(r));
+    // create thread worker
+    threads_.emplace_back([this, id = i] {
+      std::vector<int> steal_index;
+      for (int j = 1; j < concurrency_; j++) {
+          steal_index.emplace_back((id + j) % concurrency_);
+      }
+      printf("id: %d, steal: %d, %d, %d, %d, %d, %d, %d\n", id, steal_index[0], steal_index[1], steal_index[2], steal_index[3], steal_index[4], steal_index[5], steal_index[6]);
+      // in BATCH mode, wait for signal
+      while (status_ == PoolStatus::PREPARE) {
+      };
+      // enter main loop of polling and execution
+      while (true) {
+        Task next_task;
+        bool has_next_task = false;
+        {
+          // wait for either a task available, or exit signal
+          do {
+            {
+              std::unique_lock<std::mutex> lock(resources_[id]->pop_mtx);
+              has_next_task = resources_[id]->queue.pop(next_task);
+            }
+            if (!has_next_task) {
+              std::this_thread::yield();
+            }
+          } while (!has_next_task && status_ != PoolStatus::EXIT);
+
+          //          std::unique_lock<std::mutex>
+          //          lock(resources_[id]->pop_mtx);
+          //          resources_[id]->cv.wait(lock, [this, id = id, &next_task,
+          //          &has_next_task]() -> bool {
+          //            has_next_task = resources_[id]->queue.pop(next_task);
+          //            return status_ == PoolStatus::EXIT || has_next_task;
+          //          });
+          if (!has_next_task && status_ == PoolStatus::EXIT) {
+            // this pool is about to be destroyed
+            return;
+          }
+        }
+        next_task();
+        // there is no add_fetch available
+        int post_increment = finish_count_.fetch_add(1) + 1;
+        // printf("Finished task %d\n", post_increment);
+        // fflush(stdout);
+        if (post_increment == submit_count_) {
+          // notify the WaitUntilFinished() caller
+          cv_count_.notify_all();
+        }
+      }
+    });
+  }
+}
+
+LocalFinePoolNaiveSteal::~LocalFinePoolNaiveSteal() {
+  // force signal and clear
+  Exit();
+  // harvest all worker threads
+  for (auto& worker : threads_) {
+    worker.join();
+  }
+}
+
+void LocalFinePoolNaiveSteal::Submit(Task task) {
+  assert(status_ != PoolStatus::EXIT);
+  // Round-robin load balancer
+  int robin = submit_count_.fetch_add(1);  // atmoic add
+  int i = robin % concurrency_;
+  {
+    // does this create contention? but seems unavoidable
+    std::unique_lock<std::mutex> lock(resources_[i]->push_mtx);
+    resources_[i]->queue.push(std::move(task));
+    // printf("Pushed task %d\n", robin);
+    // fflush(stdout);
+  }
+  resources_[i]->cv.notify_all();
+}
+
+void LocalFinePoolNaiveSteal::WaitUntilFinished() {
+  std::unique_lock<std::mutex> lock(mtx_count_);
+  cv_count_.wait(lock,
+                 [this]() -> bool { return submit_count_ == finish_count_; });
+}
+
+void LocalFinePoolNaiveSteal::Exit() {
+  status_ = PoolStatus::EXIT;
+  for (int i = 0; i < concurrency_; i++) {
+    // wake up sleeping worker
+    resources_[i]->cv.notify_all();
+  }
+}
---
+++ src/local_fine_pool_naive_steal.h
@@ -0,0 +1,42 @@
+/**
+ * @file local_fine_pool.h
+ * @expectation this header file should be compatible to compile in C++
+ * program on Linux
+ * @init_date Apr 18 2023
+ *
+ * This is a header file that specifies the second version of threadpool
+ * which is thread-local work queue with fine locking
+ */
+
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+
+#include "base_pool.h"
+#include "fine_queue.h"
+
+class LocalFinePoolNaiveSteal final : public BasePool {
+ public:
+  LocalFinePoolNaiveSteal(int concurrency, PoolType pool_type);
+
+  ~LocalFinePoolNaiveSteal();
+
+  void Submit(Task task) override;
+
+  void WaitUntilFinished() override;
+
+  void Exit();
+
+ private:
+  std::atomic<int> submit_count_{0};
+  std::atomic<int> finish_count_{0};
+  std::vector<std::thread> threads_;
+  std::vector<std::unique_ptr<PaddedResourceFine>> resources_;
+  std::mutex mtx_count_;
+  std::condition_variable cv_count_;
+};
